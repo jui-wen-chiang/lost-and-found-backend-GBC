7776bc06f6bfa07a339eaf66ff8f60e483aed8fb
jui-wen-chiang <raewen.juiwenchiang@gmail.com>
2026-02-02 11:13:23 -0500

    fix: import dj_database_url

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 from decouple import config
-# import dj_database_url
+import dj_database_url
 from pathlib import Path
 import os
 
