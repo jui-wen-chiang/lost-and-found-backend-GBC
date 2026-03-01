5fe2c78355b1d6e2d34e6c384ba997c4db412e8e
jui-wen-chiang <raewen.juiwenchiang@gmail.com>
2026-03-01 14:01:26 -0500

    feat: add category model field

--- api/models/category.py
+++ api/models/category.py
@@ -4,6 +4,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     is_active = models.BooleanField(default=True)
+    expires_day = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
