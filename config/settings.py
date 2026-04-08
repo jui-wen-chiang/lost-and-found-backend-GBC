4a3a7caabb1394c3f170d2a26dbf483105706667
jui-wen-chiang <raewen.juiwenchiang@gmail.com>
2026-04-08 16:30:34 -0400

    docs: add logging setting

--- config/settings.py
+++ config/settings.py
@@ -214,3 +214,17 @@ CACHES = {
 }
 
 SUPABASE_STORAGE_CACHE_TTL = 300 
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "ERROR",
+    },
+}
